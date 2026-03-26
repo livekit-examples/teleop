924ec9e6a38fc9707302ae0ffdabe2cc2349f320
Stephen DeRosa <stephen.derosa@livekit.io>
2026-03-26 13:05:49 -0600

    move realsense install to cmake

--- pan_tilt_demo/cmake/Dependencies.cmake
+++ pan_tilt_demo/cmake/Dependencies.cmake
@@ -7,6 +7,33 @@
 include_guard(GLOBAL)
 include(FetchContent)
 
+# ---- librealsense2 ---------------------------------------------------------
+# Follow the upstream Ubuntu installation flow from:
+# https://github.com/realsenseai/librealsense/blob/master/doc/installation.md
+#
+# The SDK is expected to be installed on the system (typically into
+# /usr/local after building from source). If you installed into a custom
+# prefix, point REALSENSE_LOCAL_INSTALL_DIR at that prefix so CMake can find
+# realsense2Config.cmake.
+set(REALSENSE_LOCAL_INSTALL_DIR "" CACHE PATH
+  "Path to a local librealsense2 install prefix")
+
+if(REALSENSE_LOCAL_INSTALL_DIR)
+  message(STATUS "Using local librealsense2 install: ${REALSENSE_LOCAL_INSTALL_DIR}")
+  list(PREPEND CMAKE_PREFIX_PATH "${REALSENSE_LOCAL_INSTALL_DIR}")
+endif()
+
+find_package(realsense2 REQUIRED)
+
+if(TARGET realsense2::realsense2)
+  set(PAN_TILT_DEMO_REALSENSE_TARGET realsense2::realsense2)
+elseif(DEFINED realsense2_LIBRARY)
+  set(PAN_TILT_DEMO_REALSENSE_TARGET ${realsense2_LIBRARY})
+else()
+  message(FATAL_ERROR
+    "Found librealsense2, but no imported target or library variable was exported.")
+endif()
+
 # ---- SCServo (Feetech serial-bus servo SDK) --------------------------------
 FetchContent_Declare(SCServo_Linux
   GIT_REPOSITORY https://github.com/adityakamath/SCServo_Linux.git
