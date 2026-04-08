585cd29bca8bd8528fbd461081708c27086f232e
Stephen DeRosa <stephen.derosa@livekit.io>
2026-04-08 15:47:03 -0600

    rm LK_LOG

--- pan_tilt_demo/include/ostream_log.h
+++ pan_tilt_demo/include/ostream_log.h
@@ -15,3 +15,37 @@ inline void WriteLine(std::ostream& stream, fmt::format_string<Args...> format,
                       Args&&... args) {
   stream << fmt::format(format, std::forward<Args>(args)...) << '\n';
 }
+
+namespace console_log {
+
+template <typename... Args>
+inline void trace(fmt::format_string<Args...> format, Args&&... args) {
+  WriteLine(std::cout, format, std::forward<Args>(args)...);
+}
+
+template <typename... Args>
+inline void debug(fmt::format_string<Args...> format, Args&&... args) {
+  WriteLine(std::cout, format, std::forward<Args>(args)...);
+}
+
+template <typename... Args>
+inline void info(fmt::format_string<Args...> format, Args&&... args) {
+  WriteLine(std::cout, format, std::forward<Args>(args)...);
+}
+
+template <typename... Args>
+inline void warn(fmt::format_string<Args...> format, Args&&... args) {
+  WriteLine(std::cerr, format, std::forward<Args>(args)...);
+}
+
+template <typename... Args>
+inline void error(fmt::format_string<Args...> format, Args&&... args) {
+  WriteLine(std::cerr, format, std::forward<Args>(args)...);
+}
+
+template <typename... Args>
+inline void critical(fmt::format_string<Args...> format, Args&&... args) {
+  WriteLine(std::cerr, format, std::forward<Args>(args)...);
+}
+
+}  // namespace console_log
